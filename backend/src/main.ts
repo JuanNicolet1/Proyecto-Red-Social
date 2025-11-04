63be43e5f09c1468242d6183fb82b1a67930befd
Juan Ignacio Nicolet <juanignacionico@gmail.com>
2025-11-04 18:01:54 -0300

    Primer Sprint - Actualización Back

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,8 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const allowedOrigin = process.env.FRONTEND_URL || 'http://localhost:4200';
+
   app.enableCors({
-    origin: 'http://localhost:4200',
+    origin: allowedOrigin,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
   });
   await app.listen(process.env.PORT ?? 3000);
 }
