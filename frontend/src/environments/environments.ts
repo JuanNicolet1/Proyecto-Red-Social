6dd36f870ec66718f9d25a3bc3b015804c2236ce
Juan Ignacio Nicolet <juanignacionico@gmail.com>
2025-11-04 19:38:38 -0300

    Primer Sprint - Corrección URL Environments

--- frontend/src/environments/environments.ts
+++ frontend/src/environments/environments.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://proyecto-red-social-production-b80e.up.railway.app/',
+  apiUrl: 'https://proyecto-red-social-production-b80e.up.railway.app',
 };
