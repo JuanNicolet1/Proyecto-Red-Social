6b004386307963898575390f3786016220122d1b
Juan Ignacio Nicolet <juanignacionico@gmail.com>
2025-11-26 13:42:58 -0300

    Cambios url 2

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,23 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const allowedOrigin = process.env.FRONTEND_URL || 'http://localhost:4200';
 
   app.enableCors({
-    origin: [
-      'https://proyecto-redsocial.onrender.com',
-      'http://localhost:4200',
-    ],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    allowedHeaders: 'Content-Type, Authorization',
+    origin: allowedOrigin,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
-
-  const port = parseInt(process.env.PORT || '3000') || 3000;
-await app.listen(port, '0.0.0.0', () => {
-  console.log(`Backend corriendo en puerto ${port}`);
-});
-
-
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
-
